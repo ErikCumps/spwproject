9632a6fd057c013ea64e45152667836d16a2746c
ErikCumps <erik.cumps@gmail.com>
2018-09-16 23:27:22 +0200

    Updated copyright statement years for modified files
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

--- src/warcab/src/gui/gui_private.h
+++ src/warcab/src/gui/gui_private.h
@@ -1,7 +1,7 @@
 /** \file
  * The SPWaW war cabinet - GUI - private header file.
  *
- * Copyright (C) 2005-2016 Erik Cumps <erik.cumps@gmail.com>
+ * Copyright (C) 2005-2018 Erik Cumps <erik.cumps@gmail.com>
  *
  * License: GPL v2
  */
