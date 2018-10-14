3da3f3f9be8f49c5445ace8b8b0260420208dd57
ErikCumps <erik.cumps@gmail.com>
2018-10-14 23:11:25 +0200

    SPWW2 OOB data field values exploration.
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

--- src/spwawlib/src/spwoob/raw.cpp
+++ src/spwawlib/src/spwoob/raw.cpp
@@ -25,18 +25,16 @@ spwoob_dump_raw_data (RAWOOB *raw, BYTE id, char *base)
 	file = fopen (name, "w");
 	if (file) {
 		fprintf (file, "RAWOOB: size=0x%8.8x (%u)\n", sizeof(*raw), sizeof(*raw));
-		fprintf (file, "RAWOOB-cmt     : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,cmt), sizeof(raw->cmt), sizeof(raw->cmt));
-		fprintf (file, "RAWOOB-  w     : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,w), sizeof(raw->w), sizeof(raw->w));
-		fprintf (file, "RAWOOB-  w-name: 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,w.name), sizeof(raw->w.name), sizeof(raw->w.name));
-		fprintf (file, "RAWOOB-  w-d00 : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,w.__data00), sizeof(raw->w.__data00), sizeof(raw->w.__data00));
-		//fprintf (file, "RAWOOB-pad0    : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,__padding0), sizeof(raw->__padding0), sizeof(raw->__padding0));
-		fprintf (file, "RAWOOB-  u     : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,u), sizeof(raw->u), sizeof(raw->u));
-		fprintf (file, "RAWOOB-  u-name: 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,u.name), sizeof(raw->u.name), sizeof(raw->u.name));
-		fprintf (file, "RAWOOB-  u-d00 : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,u.__data00), sizeof(raw->u.__data00), sizeof(raw->u.__data00));
-		//fprintf (file, "RAWOOB-pad1    : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,__padding1), sizeof(raw->__padding1), sizeof(raw->__padding1));
-		fprintf (file, "RAWOOB-  f     : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,f), sizeof(raw->f), sizeof(raw->f));
-		fprintf (file, "RAWOOB-  f-name: 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,f.name), sizeof(raw->f.name), sizeof(raw->f.name));
-		fprintf (file, "RAWOOB-  f-d00 : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,f.__data00), sizeof(raw->f.__data00), sizeof(raw->f.__data00));
+		fprintf (file, "RAWOOB-cmt       : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,cmt), sizeof(raw->cmt), sizeof(raw->cmt));
+		fprintf (file, "RAWOOB-  w       : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,w), sizeof(raw->w), sizeof(raw->w));
+		fprintf (file, "RAWOOB-  w-name  : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,w.name), sizeof(raw->w.name), sizeof(raw->w.name));
+		fprintf (file, "RAWOOB-  w-wclass: 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,w.wclass), sizeof(raw->w.wclass), sizeof(raw->w.wclass));
+		fprintf (file, "RAWOOB-  u       : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,u), sizeof(raw->u), sizeof(raw->u));
+		fprintf (file, "RAWOOB-  u-name  : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,u.name), sizeof(raw->u.name), sizeof(raw->u.name));
+		fprintf (file, "RAWOOB-  u-uclass: 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,u.uclass), sizeof(raw->u.uclass), sizeof(raw->u.uclass));
+		fprintf (file, "RAWOOB-  f       : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,f), sizeof(raw->f), sizeof(raw->f));
+		fprintf (file, "RAWOOB-  f-name  : 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,f.name), sizeof(raw->f.name), sizeof(raw->f.name));
+		fprintf (file, "RAWOOB-  f-nation: 0x%8.8x - size=0x%8.8x (%u)\n", offsetof(RAWOOB,f.nation), sizeof(raw->f.nation), sizeof(raw->f.nation));
 		fclose (file);
 	}
 
@@ -54,37 +52,39 @@ spwoob_dump_raw_data (RAWOOB *raw, BYTE id, char *base)
 	if (file) {
 		fprintf (file,
 			"id,name,"
-			"B00,B01,B02,B03,B04,B05,B06,B07,B08,B09,B10,B11,B12,B13,"
-			"X0,X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11"
+			"wclass,size,"
+			"B00,"
+			"warhead,pen.HE,pen.AP,kill.HE,kill.AP,accuracy,range_max,range_APCR,pen_APCR,pen_HEAT,"
+			"X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12"
 			"\n");
 
 		for (i=1; i<SPWOOB_WCNT; i++) {
 			azstrcpy (raw->w.name[i].data, azsname);
 			fprintf (file,
 				"%d,%s,"
-				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
+				"%u,%u,"
+				"%u,"
+				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
 				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u"
 				"\n",
 				i, azsname,
-				raw->w.__data00[i], raw->w.__data01[i], raw->w.__data02[i], raw->w.__data03[i],
-				raw->w.__data04[i], raw->w.__data05[i], raw->w.__data06[i], raw->w.__data07[i],
-				raw->w.__data08[i], raw->w.__data09[i], raw->w.__data10[i], raw->w.__data11[i],
-				raw->w.__data12[i], raw->w.__data13[i],
-				raw->w.__datax[11*i+0], raw->w.__datax[11*i+1], raw->w.__datax[11*i+2], raw->w.__datax[11*i+3],
-				raw->w.__datax[11*i+4], raw->w.__datax[11*i+5], raw->w.__datax[11*i+6], raw->w.__datax[11*i+7],
-				raw->w.__datax[11*i+8], raw->w.__datax[11*i+9], raw->w.__datax[11*i+10]
+				raw->w.wclass[i], raw->w.size[i],
+				raw->w.__data0[i],
+				raw->w.warhead[i],
+				raw->w.pen[i].HE, raw->w.pen[i].AP,
+				raw->w.kill[i].HE, raw->w.kill[i].AP,
+				raw->w.accuracy[i], raw->w.rng_max[i], raw->w.rng_APCR[i],
+				raw->w.pen_APCR[i], raw->w.pen_HEAT[i],
+				raw->w.__dataX[ 0*SPWOOB_WCNT+i], raw->w.__dataX[ 1*SPWOOB_WCNT+i],
+				raw->w.__dataX[ 2*SPWOOB_WCNT+i], raw->w.__dataX[ 3*SPWOOB_WCNT+i],
+				raw->w.__dataX[ 4*SPWOOB_WCNT+i], raw->w.__dataX[ 5*SPWOOB_WCNT+i],
+				raw->w.__dataX[ 6*SPWOOB_WCNT+i], raw->w.__dataX[ 7*SPWOOB_WCNT+i],
+				raw->w.__dataX[ 8*SPWOOB_WCNT+i], raw->w.__dataX[ 9*SPWOOB_WCNT+i],
+				raw->w.__dataX[10*SPWOOB_WCNT+i], raw->w.__dataX[11*SPWOOB_WCNT+i]
 				);
 		}
 		fclose (file);
 	}
-
-	//memset (name, 0, sizeof (name));
-	//snprintf (name, sizeof (name) - 1, "raw_%s_%02u_padding0.dat", base, id);
-	//file = fopen (name, "w");
-	//if (file) {
-	//	fwrite (&(raw->__padding0), sizeof(raw->__padding0), 1, file);
-	//	fclose (file);
-	//}
 
 	memset (name, 0, sizeof (name));
 	snprintf (name, sizeof (name) - 1, "raw_%s_%02u_u.csv", base, id);
@@ -92,12 +92,18 @@ spwoob_dump_raw_data (RAWOOB *raw, BYTE id, char *base)
 	if (file) {
 		fprintf (file,
 			"id,name,"
-			"B00,B01,B02,B03,B04,B05,B06,B07,B08,B09,"
-			"B10,B11,B12,B13,B14,B15,B16,B17,B18,B19,"
+			"uclass,arm_SK,size,crew,"
+			"wpn_w1,wpn_w2,wpn_w3,wpn_w4,"
+			"wpn_ammo_HE1,wpn_ammo_AP1,wpn_ammo_HE2,wpn_ammo_AP2,wpn_ammo_HE3,wpn_ammo_AP3,wpn_ammo_HE4,wpn_ammo_AP4,"
+			"speed,"
+			"B17,B18,B19,"
 			"B20,B21,B22,B23,B24,B25,B26,B27,B28,B29,"
 			"B30,B31,B32,B33,B34,B35,B36,B37,B38,B39,"
-			"B40,B41,B42,B43,B44,B45,B46,B47,B48,B49,"
-			"B50,B51,B52,B53,B54,B55,B56,B57,B58,B59,"
+			"B40,B41,B42,B43,"
+			"radio,start_yr,end_yr,cost,nation,"
+			//"B50,B51,"
+			"lbm,"
+			"B52,B53,B54,B55,B56,B57,B58,B59,"
 			"B60,B61,B62,B63,B64,B65,B66,B67,B68,B69,"
 			"B70,B71,B72,B73,B74,B75,B76,B77,B78,B79,"
 			"B80,B81,B82,B83,B84,B85,B86,B87,B88,B89,"
@@ -109,12 +115,18 @@ spwoob_dump_raw_data (RAWOOB *raw, BYTE id, char *base)
 			azstrcpy (raw->u.name[i].data, azsname);
 			fprintf (file,
 				"%d,%s,"
+				"%u,%u,%u,%u,"
+				"%u,%u,%u,%u,"
+				"%u,%u,%u,%u,%u,%u,%u,%u,"
+				"%u,"
+				"%u,%u,%u,"
 				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
 				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
-				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
-				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
-				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
-				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
+				"%u,%u,%u,%u,"
+				"%u,%u,%u,%u,%u,"
+				//"%u,%u,"
+				"%u,"
+				"%u,%u,%u,%u,%u,%u,%u,%u,"
 				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
 				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
 				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
@@ -122,17 +134,21 @@ spwoob_dump_raw_data (RAWOOB *raw, BYTE id, char *base)
 				"%u,%u,%u,%u,%u,%u"
 				"\n",
 				i, azsname,
-				raw->u.__data00[i], raw->u.__data01[i], raw->u.__data02[i], raw->u.__data03[i], raw->u.__data04[i],
-				raw->u.__data05[i], raw->u.__data06[i], raw->u.__data07[i], raw->u.__data08[i], raw->u.__data09[i],
-				raw->u.__data10[i], raw->u.__data11[i], raw->u.__data12[i], raw->u.__data13[i], raw->u.__data14[i],
-				raw->u.__data15[i], raw->u.__data16[i], raw->u.__data17[i], raw->u.__data18[i], raw->u.__data19[i],
+				raw->u.uclass[i], raw->u.arm_SK[i], raw->u.size[i], raw->u.crew[i],
+				raw->u.wpn[i].w1, raw->u.wpn[i].w2, raw->u.wpn[i].w3, raw->u.wpn[i].w4,
+				raw->u.wpn_ammo[i].HE1, raw->u.wpn_ammo[i].AP1, raw->u.wpn_ammo[i].HE2, raw->u.wpn_ammo[i].AP2,
+				raw->u.wpn_ammo[i].HE3, raw->u.wpn_ammo[i].AP3, raw->u.wpn_ammo[i].HE4, raw->u.wpn_ammo[i].AP4,
+				raw->u.speed[i],
+				raw->u.__data17[i], raw->u.__data18[i], raw->u.__data19[i],
 				raw->u.__data20[i], raw->u.__data21[i], raw->u.__data22[i], raw->u.__data23[i], raw->u.__data24[i],
 				raw->u.__data25[i], raw->u.__data26[i], raw->u.__data27[i], raw->u.__data28[i], raw->u.__data29[i],
 				raw->u.__data30[i], raw->u.__data31[i], raw->u.__data32[i], raw->u.__data33[i], raw->u.__data34[i],
 				raw->u.__data35[i], raw->u.__data36[i], raw->u.__data37[i], raw->u.__data38[i], raw->u.__data39[i],
-				raw->u.__data40[i], raw->u.__data41[i], raw->u.__data42[i], raw->u.__data43[i], raw->u.__data44[i],
-				raw->u.__data45[i], raw->u.__data46[i], raw->u.__data47[i], raw->u.__data48[i], raw->u.__data49[i],
-				raw->u.__data50[i], raw->u.__data51[i], raw->u.__data52[i], raw->u.__data53[i], raw->u.__data54[i],
+				raw->u.__data40[i], raw->u.__data41[i], raw->u.__data42[i], raw->u.__data43[i],
+				raw->u.radio[i], raw->u.start_yr[i], raw->u.end_yr[i], raw->u.cost[i], raw->u.nation[i],
+				//raw->u.__data50[i], raw->u.__data51[i],
+				raw->u.lbm[i],
+				raw->u.__data52[i], raw->u.__data53[i], raw->u.__data54[i],
 				raw->u.__data55[i], raw->u.__data56[i], raw->u.__data57[i], raw->u.__data58[i], raw->u.__data59[i],
 				raw->u.__data60[i], raw->u.__data61[i], raw->u.__data62[i], raw->u.__data63[i], raw->u.__data64[i],
 				raw->u.__data65[i], raw->u.__data66[i], raw->u.__data67[i], raw->u.__data68[i], raw->u.__data69[i],
@@ -149,42 +165,43 @@ spwoob_dump_raw_data (RAWOOB *raw, BYTE id, char *base)
 		fclose (file);
 	}
 
-	//memset (name, 0, sizeof (name));
-	//snprintf (name, sizeof (name) - 1, "raw_%s_%02u_padding1.dat", base, id);
-	//file = fopen (name, "w");
-	//if (file) {
-	//	fwrite (&(raw->__padding1), sizeof(raw->__padding1), 1, file);
-	//	fclose (file);
-	//}
-
 	memset (name, 0, sizeof (name));
 	snprintf (name, sizeof (name) - 1, "raw_%s_%02u_f.csv", base, id);
 	file = fopen (name, "w");
 	if (file) {
 		fprintf (file,
 			"id,name,"
-			"B00,B01,B02,B03,B04,B05,B06,B07,B08,B09,B10,B11,B12,B13,B14,"
-			"UR1,UR2,UR3,UR4,UR5,UR6,UR7,UR8,UR9,UR10,"
-			"UC1,UC2,UC3,UC4,UC5,UC6,UC7,UC8,UC9,UC10"
+			"nation,start_yr,end_yr,pscr,type,"
+			"URID[1],URID[2],URID[3],URID[4],URID[5],URID[6],URID[7],URID[8],URID[9],URID[10],"
+			"start_mo,end_mo,exp_mod,mor_mod,"
+			"X01,X02,X03,X04,X05,X06,X07,X08,X09,X10,X11,X12,X13,X14,X15,X16"
 			"\n");
 
 		for (i=1; i<SPWOOB_FCNT; i++) {
 			azstrcpy (raw->f.name[i].data, azsname);
 			fprintf (file,
 				"%d,%s,"
-				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
+				"%u,%u,%u,%u,%u,"
 				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,"
-				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u"
+				"%u,%u,%u,%u,"
+				"%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u"
 				"\n",
 				i, azsname,
-				raw->f.__data00[i], raw->f.__data01[i], raw->f.__data02[i], raw->f.__data03[i],
-				raw->f.__data04[i], raw->f.__data05[i], raw->f.__data06[i], raw->f.__data07[i],
-				raw->f.__data08[i], raw->f.__data09[i], raw->f.__data10[i], raw->f.__data11[i],
-				raw->f.__data12[i], raw->f.__data13[i], raw->f.__data14[i],
-				raw->f.urid[i].data[0], raw->f.urid[i].data[1], raw->f.urid[i].data[2], raw->f.urid[i].data[3], raw->f.urid[i].data[4],
-				raw->f.urid[i].data[5], raw->f.urid[i].data[6], raw->f.urid[i].data[7], raw->f.urid[i].data[8], raw->f.urid[i].data[9],
-				raw->f.ucnt[i].data[0], raw->f.ucnt[i].data[1], raw->f.ucnt[i].data[2], raw->f.ucnt[i].data[3], raw->f.ucnt[i].data[4],
-				raw->f.ucnt[i].data[5], raw->f.ucnt[i].data[6], raw->f.ucnt[i].data[7], raw->f.ucnt[i].data[8], raw->f.ucnt[i].data[9]
+				raw->f.nation[i], raw->f.start_yr[i], raw->f.end_yr[i], raw->f.pscr[i], raw->f.type[i],
+				raw->f.urid[i].dat[0], raw->f.urid[i].dat[1],
+				raw->f.urid[i].dat[2], raw->f.urid[i].dat[3],
+				raw->f.urid[i].dat[4], raw->f.urid[i].dat[5],
+				raw->f.urid[i].dat[6], raw->f.urid[i].dat[7],
+				raw->f.urid[i].dat[8], raw->f.urid[i].dat[9],
+				raw->f.start_mo[i], raw->f.end_mo[i], raw->f.exp_mod[i], raw->f.mor_mod[i],
+				raw->f.__dataX[ 0*SPWOOB_FCNT+i], raw->f.__dataX[ 1*SPWOOB_FCNT+i],
+				raw->f.__dataX[ 2*SPWOOB_FCNT+i], raw->f.__dataX[ 3*SPWOOB_FCNT+i],
+				raw->f.__dataX[ 4*SPWOOB_FCNT+i], raw->f.__dataX[ 5*SPWOOB_FCNT+i],
+				raw->f.__dataX[ 6*SPWOOB_FCNT+i], raw->f.__dataX[ 7*SPWOOB_FCNT+i],
+				raw->f.__dataX[ 8*SPWOOB_FCNT+i], raw->f.__dataX[ 9*SPWOOB_FCNT+i],
+				raw->f.__dataX[10*SPWOOB_FCNT+i], raw->f.__dataX[11*SPWOOB_FCNT+i],
+				raw->f.__dataX[12*SPWOOB_FCNT+i], raw->f.__dataX[13*SPWOOB_FCNT+i],
+				raw->f.__dataX[14*SPWOOB_FCNT+i], raw->f.__dataX[15*SPWOOB_FCNT+i]
 				);
 		}
 		fclose (file);
--- src/spwawlib/src/spwoob/raw.h
+++ src/spwawlib/src/spwoob/raw.h
@@ -26,66 +26,29 @@ typedef struct s_CMT {
 typedef struct s_RAWOOB {
 	CMT	cmt;
         struct s_w {
-		//NAME	name		[SPWOOB_WCNT-1];
 		NAME	name		[SPWOOB_WCNT];
-		//BYTE	wclass		[SPWOOB_WCNT];
-		////BYTE	__data0		[SPWOOB_WCNT];		/* Sym.			*/
-		//BYTE	size		[SPWOOB_WCNT];
-		//BYTE	warhead		[SPWOOB_WCNT];
-		//struct s_pen {
-		//	BYTE	HE;
-		//	BYTE	AP;
-		//}	pen		[SPWOOB_WCNT];
-		//USHORT	kill		[SPWOOB_WCNT];
-		//BYTE	accuracy	[SPWOOB_WCNT];
-		//BYTE	rng_max		[SPWOOB_WCNT];
-		//BYTE	rng_APCR	[SPWOOB_WCNT];
-		//BYTE	pen_APCR	[SPWOOB_WCNT];
-		//BYTE	pen_HEAT	[SPWOOB_WCNT];
-		//USHORT	sound		[SPWOOB_WCNT];
-		BYTE __data00		[SPWOOB_WCNT];
-		BYTE __data01		[SPWOOB_WCNT];
-		BYTE __data02		[SPWOOB_WCNT];
-		BYTE __data03		[SPWOOB_WCNT];
-		BYTE __data04		[SPWOOB_WCNT];
-		BYTE __data05		[SPWOOB_WCNT];
-		BYTE __data06		[SPWOOB_WCNT];
-		BYTE __data07		[SPWOOB_WCNT];
-		BYTE __data08		[SPWOOB_WCNT];
-		BYTE __data09		[SPWOOB_WCNT];
-		BYTE __data10		[SPWOOB_WCNT];
-		BYTE __data11		[SPWOOB_WCNT];
-		BYTE __data12		[SPWOOB_WCNT];
-		BYTE __data13		[SPWOOB_WCNT];
-		//BYTE	__data2		[7*SPWOOB_WCNT];	/* x1 ... x7		*/
-		BYTE	__datax		[11*SPWOOB_WCNT];	/* x1 ... x11		*/
+		BYTE	wclass		[SPWOOB_WCNT];
+		BYTE	size		[SPWOOB_WCNT];
+		BYTE	__data0		[SPWOOB_WCNT];		/* Unknown data */
+		BYTE	warhead		[SPWOOB_WCNT];
+		struct s_pen {
+			BYTE	HE;
+			BYTE	AP;
+		}	pen		[SPWOOB_WCNT];
+		struct s_kill {
+			BYTE	HE;
+			BYTE	AP;
+		}	kill		[SPWOOB_WCNT];
+		BYTE	accuracy	[SPWOOB_WCNT];
+		BYTE	rng_max		[SPWOOB_WCNT];
+		BYTE	rng_APCR	[SPWOOB_WCNT];
+		BYTE	pen_APCR	[SPWOOB_WCNT];
+		BYTE	pen_HEAT	[SPWOOB_WCNT];
+		BYTE	__dataX		[12*SPWOOB_WCNT];	/* X1 ... X12		*/
 	} w;
-	//BYTE	__padding0		[15];
 	struct s_u {
-		//NAME	name		[SPWOOB_UCNT-1];
 		NAME	name		[SPWOOB_UCNT];
-		//BYTE	uclass		[SPWOOB_UCNT];
-		//BYTE	arm_SK		[SPWOOB_UCNT];
-		//BYTE	size		[SPWOOB_UCNT];
-		//BYTE	crew		[SPWOOB_UCNT];
-		//struct	s_wpn {
-		//	BYTE	w1;
-		//	BYTE	w2;
-		//	BYTE	w3;
-		//	BYTE	w4;
 
-		//}	wpn		[SPWOOB_UCNT];
-		//struct s_wpn_ammo {
-		//	BYTE	HE1;
-		//	BYTE	AP1;
-		//	BYTE	HE2;
-		//	BYTE	AP2;
-		//	BYTE	HE3;
-		//	BYTE	AP3;
-		//	BYTE	HE4;
-		//	BYTE	AP4;
-		//}	wpn_ammo	[SPWOOB_UCNT];
-		//BYTE	speed		[SPWOOB_UCNT];
 		//struct s_arm {
 		//	BYTE	FH;
 		//	BYTE	SH;
@@ -105,12 +68,7 @@ typedef struct s_RAWOOB {
 		//	BYTE	ST;
 		//	BYTE	RT;
 		//}	slp		[SPWOOB_UCNT];
-		//BYTE	radio		[SPWOOB_UCNT];
-		//BYTE	start_yr	[SPWOOB_UCNT];
-		//BYTE	end_yr		[SPWOOB_UCNT];
-		//BYTE	cost		[SPWOOB_UCNT];
-		//BYTE	nation		[SPWOOB_UCNT];
-		//USHORT	lbm		[SPWOOB_UCNT];
+
 		//BYTE	irvis		[SPWOOB_UCNT];
 		//BYTE	fc		[SPWOOB_UCNT];
 		//BYTE	wpn1_APCRammo	[SPWOOB_UCNT];
@@ -136,23 +94,29 @@ typedef struct s_RAWOOB {
 		//BYTE	__data3		[2*SPWOOB_UCNT];	/* x1 ... x2		*/
 		//USHORT	msound		[SPWOOB_UCNT];
 		//BYTE	__data4		[28*SPWOOB_UCNT];	/* z1 ... z28		*/
-		BYTE __data00		[SPWOOB_UCNT];
-		BYTE __data01		[SPWOOB_UCNT];
-		BYTE __data02		[SPWOOB_UCNT];
-		BYTE __data03		[SPWOOB_UCNT];
-		BYTE __data04		[SPWOOB_UCNT];
-		BYTE __data05		[SPWOOB_UCNT];
-		BYTE __data06		[SPWOOB_UCNT];
-		BYTE __data07		[SPWOOB_UCNT];
-		BYTE __data08		[SPWOOB_UCNT];
-		BYTE __data09		[SPWOOB_UCNT];
-		BYTE __data10		[SPWOOB_UCNT];
-		BYTE __data11		[SPWOOB_UCNT];
-		BYTE __data12		[SPWOOB_UCNT];
-		BYTE __data13		[SPWOOB_UCNT];
-		BYTE __data14		[SPWOOB_UCNT];
-		BYTE __data15		[SPWOOB_UCNT];
-		BYTE __data16		[SPWOOB_UCNT];
+
+		BYTE	uclass		[SPWOOB_UCNT];
+		BYTE	arm_SK		[SPWOOB_UCNT];
+		BYTE	size		[SPWOOB_UCNT];
+		BYTE	crew		[SPWOOB_UCNT];
+		struct	s_wpn {
+			BYTE	w1;
+			BYTE	w2;
+			BYTE	w3;
+			BYTE	w4;
+		}	wpn		[SPWOOB_UCNT];
+		struct s_wpn_ammo {
+			BYTE	HE1;
+			BYTE	AP1;
+			BYTE	HE2;
+			BYTE	AP2;
+			BYTE	HE3;
+			BYTE	AP3;
+			BYTE	HE4;
+			BYTE	AP4;
+		}	wpn_ammo	[SPWOOB_UCNT];
+		BYTE	speed		[SPWOOB_UCNT];
+		//
 		BYTE __data17		[SPWOOB_UCNT];
 		BYTE __data18		[SPWOOB_UCNT];
 		BYTE __data19		[SPWOOB_UCNT];
@@ -180,14 +144,17 @@ typedef struct s_RAWOOB {
 		BYTE __data41		[SPWOOB_UCNT];
 		BYTE __data42		[SPWOOB_UCNT];
 		BYTE __data43		[SPWOOB_UCNT];
-		BYTE __data44		[SPWOOB_UCNT];
-		BYTE __data45		[SPWOOB_UCNT];
-		BYTE __data46		[SPWOOB_UCNT];
-		BYTE __data47		[SPWOOB_UCNT];
-		BYTE __data48		[SPWOOB_UCNT];
-		BYTE __data49		[SPWOOB_UCNT];
-		BYTE __data50		[SPWOOB_UCNT];
-		BYTE __data51		[SPWOOB_UCNT];
+		//
+		BYTE	radio		[SPWOOB_UCNT];
+		BYTE	start_yr	[SPWOOB_UCNT];
+		BYTE	end_yr		[SPWOOB_UCNT];
+		USHORT	cost		[SPWOOB_UCNT];
+		BYTE	nation		[SPWOOB_UCNT];
+		//
+		//BYTE __data50		[SPWOOB_UCNT];
+		//BYTE __data51		[SPWOOB_UCNT];
+		USHORT	lbm		[SPWOOB_UCNT];
+		//
 		BYTE __data52		[SPWOOB_UCNT];
 		BYTE __data53		[SPWOOB_UCNT];
 		BYTE __data54		[SPWOOB_UCNT];
@@ -243,39 +210,21 @@ typedef struct s_RAWOOB {
 		BYTE __data104		[SPWOOB_UCNT];
 		BYTE __data105		[SPWOOB_UCNT];
 	} u;
-	//BYTE	__padding1		[15];
 	struct s_f {
-		//NAME	name		[SPWOOB_FCNT-1];
 		NAME	name		[SPWOOB_FCNT];
-		//BYTE	nation		[SPWOOB_FCNT];
-		//BYTE	start_mo	[SPWOOB_FCNT];
-		//BYTE	special		[SPWOOB_FCNT];
-		//BYTE	__data0		[8*SPWOOB_FCNT];	/* x1 ... x8		*/
-		//BYTE	start_yr	[SPWOOB_FCNT];
-		//BYTE	end_yr		[SPWOOB_FCNT];
-		//BYTE	purchscrn	[SPWOOB_FCNT];
-		//BYTE	type		[SPWOOB_FCNT];
-		BYTE __data00		[SPWOOB_FCNT];
-		BYTE __data01		[SPWOOB_FCNT];
-		BYTE __data02		[SPWOOB_FCNT];
-		BYTE __data03		[SPWOOB_FCNT];
-		BYTE __data04		[SPWOOB_FCNT];
-		BYTE __data05		[SPWOOB_FCNT];
-		BYTE __data06		[SPWOOB_FCNT];
-		BYTE __data07		[SPWOOB_FCNT];
-		BYTE __data08		[SPWOOB_FCNT];
-		BYTE __data09		[SPWOOB_FCNT];
-		BYTE __data10		[SPWOOB_FCNT];
-		BYTE __data11		[SPWOOB_FCNT];
-		BYTE __data12		[SPWOOB_FCNT];
-		BYTE __data13		[SPWOOB_FCNT];
-		BYTE __data14		[SPWOOB_FCNT];
+		BYTE	nation		[SPWOOB_FCNT];
+		BYTE	start_yr	[SPWOOB_FCNT];
+		BYTE	end_yr		[SPWOOB_FCNT];
+		BYTE	pscr		[SPWOOB_FCNT];
+		BYTE	type		[SPWOOB_FCNT];
 		struct s_urid {
-			USHORT	data[SPWOOB_FMUCNT];
+			USHORT	dat[SPWOOB_FMUCNT];
 		}	urid		[SPWOOB_FCNT];
-		struct s_ucnt {
-			BYTE	data[SPWOOB_FMUCNT];
-		}	ucnt		[SPWOOB_FCNT];
+		BYTE	start_mo	[SPWOOB_FCNT];
+		BYTE	end_mo		[SPWOOB_FCNT];
+		BYTE	exp_mod		[SPWOOB_FCNT];
+		BYTE	mor_mod		[SPWOOB_FCNT];
+		BYTE	__dataX		[16*SPWOOB_FCNT];	/* X1 ... X16 */
 	} f;
 } RAWOOB;
 
