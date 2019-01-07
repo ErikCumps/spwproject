2b52b11b8f9817ad43697bd7c898cbe6bd08bf82
ErikCumps <erik.cumps@gmail.com>
2019-01-07 21:15:05 +0100

    Use stack iso heap for temp storage while loading older snapshot data.
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

--- src/spwawlib/src/snapshot/snapfile_v10.cpp
+++ src/spwawlib/src/snapshot/snapfile_v10.cpp
@@ -17,24 +17,21 @@ SPWAW_ERROR
 snapshot_load_v10_info_header (int fd, SNAP_INFO *hdr)
 {
 	SPWAW_ERROR	rc = SPWERR_OK;
-	SNAP_INFO_V10	*hdr_v10;
+	SNAP_INFO_V10	hdr_v10;
 
 	if (!hdr) return (SPWERR_NULLARG);
 
-	hdr_v10 = safe_malloc (SNAP_INFO_V10); COOMGOTO (hdr_v10, "SNAP_INFO_V10", handle_error);
-
-	if (!bread (fd, (char *)hdr_v10, sizeof (SNAP_INFO_V10), false))
+	if (!bread (fd, (char *)&hdr_v10, sizeof (SNAP_INFO_V10), false))
 		FAILGOTO (SPWERR_FRFAILED, "bread(snapshot info hdr v10) failed", handle_error);
 
 	/* A V10 snapshot info header only lacks the battle type at the end.
 	 * The only supported battle type for V10 snapshots is the SPWAW_CAMPAIGN_BATTLE,
 	 * so a quick copy and fix up is all we need :)
 	 */
-	memcpy (hdr, hdr_v10, sizeof (SNAP_INFO_V10));
+	memcpy (hdr, &hdr_v10, sizeof (SNAP_INFO_V10));
 	hdr->type = SPWAW_CAMPAIGN_BATTLE;
 
 handle_error:
-	if (hdr_v10) safe_free (hdr_v10);
 	return (rc);
 }
 
--- src/spwawlib/src/snapshot/snapfile_v11.cpp
+++ src/spwawlib/src/snapshot/snapfile_v11.cpp
@@ -16,14 +16,12 @@ SPWAW_ERROR
 snapshot_load_v11_snap (int fd, SNAP_HEADER *hdr, SNAP *snap)
 {
 	SPWAW_ERROR	rc = SPWERR_OK;
-	SNAP_V11	*snap_v11;
+	SNAP_V11	snap_v11;
 	CBIO		cbio;
 
 	if (!snap) return (SPWERR_NULLARG);
 
-	snap_v11 = safe_malloc (SNAP_V11); COOMGOTO (snap_v11, "SNAP_V11", handle_error);
-
-	cbio.data = (char *)snap_v11; cbio.size = hdr->snap.size; cbio.comp = &(hdr->snap.comp);
+	cbio.data = (char *)&snap_v11; cbio.size = hdr->snap.size; cbio.comp = &(hdr->snap.comp);
 	if (!cbread (fd, cbio, "snapshot game data v11"))
 		FAILGOTO (SPWERR_FRFAILED, "cbread(snapshot game data v11) failed", handle_error);
 
@@ -31,15 +29,14 @@ snapshot_load_v11_snap (int fd, SNAP_HEADER *hdr, SNAP *snap)
 	/* The V11 snapshot data only lacks the player1 and player2 battle results at the
 	 * end of the campaign data, so a quick copy and fix up is all we need :)
 	 */
-	memcpy (&(snap->cmt), &(snap_v11->cmt), sizeof (SNAP_CMT_V11));
-	memcpy (&(snap->b), &(snap_v11->b), sizeof (SNAP_BATTLE_V11));
-	memcpy (&(snap->c), &(snap_v11->c), sizeof (SNAP_CAMPAIGN_V11));
+	memcpy (&(snap->cmt), &(snap_v11.cmt), sizeof (SNAP_CMT_V11));
+	memcpy (&(snap->b), &(snap_v11.b), sizeof (SNAP_BATTLE_V11));
+	memcpy (&(snap->c), &(snap_v11.c), sizeof (SNAP_CAMPAIGN_V11));
 
 	/* We didn't save the battle results, they are unknown */
 	snap->c.P1result = BR_UNKNOWN;
 	snap->c.P2result = BR_UNKNOWN;
 
 handle_error:
-	if (snap_v11) safe_free (snap_v11);
 	return (rc);
 }
