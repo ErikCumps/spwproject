406b30d3bff7d0734900b960ac7246c79a4f0c4f
ErikCumps <erik.cumps@gmail.com>
2020-01-10 21:27:17 +0100

    Initial commit of unit history tracking list job API.
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

---
+++ src/spwawlib/include/spwawlib_uht_job.h
@@ -0,0 +1,98 @@
+/** \file
+ * The SPWaW Library - unit history tracking list job API.
+ *
+ * Copyright (C) 2020 Erik Cumps <erik.cumps@gmail.com>
+ *
+ * License: GPL v2
+ *
+ * \addtogroup API
+ * @{
+ */
+
+#ifndef	SPWAW_LIB_UHT_JOB_H
+#define	SPWAW_LIB_UHT_JOB_H	1
+
+#include <spwawlib_api.h>
+
+/* Forward declarations for convenience */
+typedef struct s_SPWAW_DOSSIER_UIR	SPWAW_DOSSIER_UIR;
+typedef struct s_SPWAW_BATTLE_DATE		SPWAW_BATTLE_DATE;
+typedef struct s_SPWAW_UHTE		SPWAW_UHTE;
+typedef struct s_SPWAW_UHT		SPWAW_UHT;
+typedef struct s_SPWAW_UHT_BINFO	SPWAW_UHT_BINFO;
+
+/* SPWAW UHT list job type */
+typedef enum e_SPWAW_UHT_LIST_JOB_TYPE {
+	SPWAW_UHT_LIST_CHAIN = 0,		/* Detect and count UHTE on all UHTE chains					*/
+	SPWAW_UHT_LIST_BINFO,			/* Detect UHTE on a battle info data list					*/
+} SPWAW_UHT_LIST_JOB_TYPE;
+
+/* SPWAW UHT list job result element.
+ *
+ * Note: the uhte result member content depends on the list job type:
+ * + for SPWAW_UHT_LIST_CHAIN: pointer to the final UHTE of the chain with one or more detected UHTEs
+ * + for SPWAW_UHT_LIST_BINFO: pointer to the detected UHTE
+ */
+typedef struct s_SPWAW_UHT_LISTEL {
+	SPWAW_UHTE		*uhte;		/* Detected UHTE								*/
+	unsigned long		count;		/* Number of detections for this UHTE						*/
+	void			*data;		/* Optional storage for extra data						*/
+} SPWAW_UHT_LISTEL;
+
+/* SPWAW UHT list job result */
+typedef struct s_SPWAW_UHT_LIST {
+	SPWAW_UHT_LIST_JOB_TYPE	type;		/* The SPWAW UHT list job type that generated this result			*/
+	SPWAW_UHT_LISTEL	*list;		/* List with detected UHTE							*/
+	SPWAW_UHT_LISTEL	**smap;		/* Sort map									*/
+	unsigned int		cnt;		/* Number of used elements in the list						*/
+	unsigned int		len;		/* Total length of the list (in elements)					*/
+} SPWAW_UHT_LIST;
+
+/* SPWAW UHT list sort type */
+typedef enum e_SPWAW_UHT_LIST_SORT_TYPE {
+	SPWAW_UHT_SORT_UID = 0,			/* Sort SPWAW_UHT_LISTEL on UHTE UID						*/
+	SPWAW_UHT_SORT_COUNT,			/* Sort SPWAW_UHT_LISTEL on UHTE detection count				*/
+	SPWAW_UHT_SORT_USER,			/* Sort SPWAW_UHT_LISTEL using user-provided comparison function		*/
+} SPWAW_UHT_LIST_SORT_TYPE;
+
+/* SPWAW UHT list sort comparison function. Arguments are pointers to *SPWAW_UHT_LISTEL. */
+typedef int (*SPWAW_UHT_list_cmp) (const void *a, const void *b);
+
+/* SPWAW UHT list job detection data callback */
+typedef void (*SPWAW_UHT_list_data_cb) (SPWAW_UHTE *uhte, SPWAW_BATTLE_DATE *bdate, SPWAW_DOSSIER_UIR *uir, bool first, void **ctx);
+
+/* SPWAW UHT list job descriptor */
+typedef struct s_SPWAW_UHT_LIST_JOB {
+	SPWAW_UHT_LIST_JOB_TYPE	type;		/* The SPWAW UHT list job type							*/
+	union u_src {
+		SPWAW_UHT	*uht;		/* Data source: UHT								*/
+		SPWAW_UHT_BINFO	*bid;		/* Data source: battle info data						*/
+	}	src;				/* UHT data source to perform the list job on					*/
+	USHORT			status;		/* Unit history status to detect						*/
+	SPWAW_UHT_list_data_cb	data_cb;	/* Detection data callback (can be NULL)					*/
+	SPWAW_UHT_LIST		*dst;		/* Storage for job result							*/
+} SPWAW_UHT_LIST_JOB;
+
+/*** API ***/
+
+/* SPWAW UHT list handling */
+extern SPWAWLIB_API SPWAW_ERROR	SPWAW_UHT_list_init	(SPWAW_UHT_LIST_JOB_TYPE type, unsigned int len, SPWAW_UHT_LIST **list);
+extern SPWAWLIB_API SPWAW_ERROR	SPWAW_UHT_list_free	(SPWAW_UHT_LIST **list);
+extern SPWAWLIB_API SPWAW_ERROR	SPWAW_UHT_list_add	(SPWAW_UHT_LIST *list, SPWAW_UHTE *uhte, SPWAW_UHT_LISTEL **listel);
+extern SPWAWLIB_API SPWAW_ERROR	SPWAW_UHT_list_sort	(SPWAW_UHT_LIST *list, SPWAW_UHT_LIST_SORT_TYPE type, bool ascending, SPWAW_UHT_list_cmp cmp);
+
+/* Perform an SPWAW UHT list job */
+extern SPWAWLIB_API SPWAW_ERROR	SPWAW_UHT_list_job	(SPWAW_UHT_LIST_JOB &job);
+
+/* Sort the SPWAW UHT list in a list job */
+extern SPWAWLIB_API SPWAW_ERROR	SPWAW_UHT_list_job_sort	(SPWAW_UHT_LIST_JOB &job, SPWAW_UHT_LIST_SORT_TYPE type, bool ascending, SPWAW_UHT_list_cmp cmp);
+
+/* Predefined SPWAW UHT list comparison functions */
+extern SPWAWLIB_API int	SPWAW_UHT_list_cmp_UID_ascending	(const void *a, const void *b);
+extern SPWAWLIB_API int	SPWAW_UHT_list_cmp_UID_descending	(const void *a, const void *b);
+extern SPWAWLIB_API int	SPWAW_UHT_list_cmp_CNT_ascending	(const void *a, const void *b);
+extern SPWAWLIB_API int	SPWAW_UHT_list_cmp_CNT_descending	(const void *a, const void *b);
+
+#endif	/* SPWAW_LIB_UHT_JOB_H */
+
+/** @} */
---
+++ src/spwawlib/src/api/api_uht_job.cpp
@@ -0,0 +1,242 @@
+/** \file
+ * The SPWaW Library - unit history tracking list job API implementation.
+ *
+ * Copyright (C) 2020 Erik Cumps <erik.cumps@gmail.com>
+ *
+ * License: GPL v2
+ */
+
+#include "stdafx.h"
+#include <spwawlib_uht_job.h>
+#include "common/internal.h"
+
+SPWAWLIB_API SPWAW_ERROR
+SPWAW_UHT_list_init (SPWAW_UHT_LIST_JOB_TYPE type, unsigned int len, SPWAW_UHT_LIST **list)
+{
+	SPWAW_ERROR	rc = SPWERR_OK;
+	SPWAW_UHT_LIST	*p;
+
+	CNULLARG (list);
+	*list = NULL;
+
+	p = safe_malloc (SPWAW_UHT_LIST); COOMGOTO (p, "SPWAW_UHT_LIST", handle_error);
+
+	p->type = type;
+	p->len = len;
+	p->cnt = 0;
+
+	if (p->len) {
+		p->list = safe_nmalloc (SPWAW_UHT_LISTEL, p->len);
+		COOMGOTO (p->list, "SPWAW_UHT_LISTEL list", handle_error);
+		p->smap = safe_nmalloc (SPWAW_UHT_LISTEL *, p->len);
+		COOMGOTO (p->smap, "SPWAW_UHT_LISTEL smap", handle_error);
+	}
+
+	*list = p;
+
+	return (SPWERR_OK);
+
+handle_error:
+	if (p) {
+		if (p->smap) safe_free (p->smap);
+		if (p->list) safe_free (p->list);
+		safe_free (p);
+	}
+	return (rc);
+}
+
+SPWAWLIB_API SPWAW_ERROR
+SPWAW_UHT_list_free (SPWAW_UHT_LIST **list)
+{
+	SPWAW_UHT_LIST	*p;
+
+	CNULLARG (list);
+	p = *list; *list = NULL;
+
+	if (p) {
+		if (p->smap) safe_free (p->smap);
+		if (p->list) safe_free (p->list);
+		safe_free (p);
+	}
+
+	return (SPWERR_OK);
+}
+
+SPWAWLIB_API SPWAW_ERROR
+SPWAW_UHT_list_add (SPWAW_UHT_LIST *list, SPWAW_UHTE *uhte, SPWAW_UHT_LISTEL **listel)
+{
+	unsigned int		idx;
+	SPWAW_UHT_LISTEL	*p;
+
+	CNULLARG (list); CNULLARG (uhte); CNULLARG (listel);
+	*listel = NULL;
+
+	if (list->cnt >= list->len) RWE (SPWERR_OUTOFMEM, "no room left in SPWAW_UHT_LIST");
+
+	idx = list->cnt++;
+	p = &(list->list[idx]); list->smap[idx] = p;
+	p->uhte = uhte;
+	*listel = p;
+
+	return (SPWERR_OK);
+}
+
+SPWAWLIB_API inline int
+SPWAW_UHT_list_cmp_UID_ascending (const void *a, const void *b)
+{
+	SPWAW_UHT_LISTEL	*ea = *((SPWAW_UHT_LISTEL **)a);
+	SPWAW_UHT_LISTEL	*eb = *((SPWAW_UHT_LISTEL **)b);
+
+	int fma = ea->uhte->FMID;
+	int fmb = eb->uhte->FMID;
+
+	int fsa = ea->uhte->FSID;
+	int fsb = eb->uhte->FSID;
+
+	return ((fma==fmb)?((fsa==fsb)?0:((fsa<fsb)?-1:+1)):((fma<fmb)?-1:+1));
+}
+
+SPWAWLIB_API int
+SPWAW_UHT_list_cmp_UID_descending (const void *a, const void *b)
+{
+	return (-SPWAW_UHT_list_cmp_UID_ascending(a,b));
+}
+
+SPWAWLIB_API int
+SPWAW_UHT_list_cmp_CNT_ascending (const void *a, const void *b)
+{
+	SPWAW_UHT_LISTEL	*ea = *((SPWAW_UHT_LISTEL **)a);
+	SPWAW_UHT_LISTEL	*eb = *((SPWAW_UHT_LISTEL **)b);
+
+	if (ea->count == eb->count) return (SPWAW_UHT_list_cmp_UID_ascending(a,b));
+	return ((ea->count < eb->count)?-1:+1);
+}
+
+SPWAWLIB_API int
+SPWAW_UHT_list_cmp_CNT_descending (const void *a, const void *b)
+{
+	SPWAW_UHT_LISTEL	*ea = *((SPWAW_UHT_LISTEL **)a);
+	SPWAW_UHT_LISTEL	*eb = *((SPWAW_UHT_LISTEL **)b);
+
+	if (ea->count == eb->count) return (SPWAW_UHT_list_cmp_UID_ascending(a,b));
+	return ((ea->count < eb->count)?+1:-1);
+}
+
+SPWAWLIB_API SPWAW_ERROR
+SPWAW_UHT_list_sort (SPWAW_UHT_LIST *list, SPWAW_UHT_LIST_SORT_TYPE type, bool ascending, SPWAW_UHT_list_cmp cmp)
+{
+	SPWAW_UHT_list_cmp	cmpfunc;
+
+	CNULLARG (list);
+
+	switch (type) {
+		default:
+		case SPWAW_UHT_SORT_UID:
+			if (ascending) 
+				cmpfunc = SPWAW_UHT_list_cmp_UID_ascending;
+			else
+				cmpfunc = SPWAW_UHT_list_cmp_UID_descending;
+			break;
+		case SPWAW_UHT_SORT_COUNT:
+			if (ascending) 
+				cmpfunc = SPWAW_UHT_list_cmp_CNT_ascending;
+			else
+				cmpfunc = SPWAW_UHT_list_cmp_CNT_descending;
+			break;
+		case SPWAW_UHT_SORT_USER:
+			CNULLARG (cmp);
+			cmpfunc = cmp;
+			break;
+	}
+	qsort (list->smap, list->cnt, sizeof (SPWAW_UHTE *), cmpfunc);
+
+	return (SPWERR_OK);
+}
+
+static SPWAW_ERROR
+perform_uht_chain_list_job (SPWAW_UHT_LIST_JOB &job)
+{
+	SPWAW_ERROR		rc = SPWERR_OK;
+	SPWAW_UHTE		*uhte, *uptr;
+	SPWAW_UHT_LISTEL	*ulle;
+	bool			first;
+
+	CNULLARG (job.src.uht);
+
+	rc = SPWAW_UHT_list_init (job.type, job.src.uht->icnt, &(job.dst)); ROE ("SPWAW_UHT_list_init()");
+
+	for (unsigned int i=0; i<job.src.uht->cnt; i++) {
+		uhte = job.src.uht->smap[i];
+		if (!SPWAW_UHT_is_initial (uhte)) continue;
+		if (SPWAW_UHT_is_decommissioned (uhte)) continue;
+
+		uptr = SPWAW_UHT_first (uhte, job.status);
+		if (!uptr) continue;
+
+		rc = SPWAW_UHT_list_add (job.dst, SPWAW_UHT_last (uhte), &ulle); ROE ("SPWAW_UHT_list_add()");
+
+		first = true;
+		while (uptr) {
+			ulle->count++;
+
+			if (job.data_cb) {
+				job.data_cb (uptr, &(uptr->FBI), SPWAW_UHT_lookup (uptr, &(uptr->FBI)), first, &(ulle->data));
+			}
+
+			uptr = SPWAW_UHT_next (uptr, job.status);
+			first = false;
+		}
+	}
+
+	return (SPWERR_OK);
+}
+static SPWAW_ERROR
+perform_uht_binfo_list_job (SPWAW_UHT_LIST_JOB &job)
+{
+	SPWAW_ERROR		rc = SPWERR_OK;
+	SPWAW_UHTE		*uhte;
+	SPWAW_UHT_LISTEL	*ulle;
+
+	CNULLARG (job.src.bid);
+
+	rc = SPWAW_UHT_list_init (job.type, job.src.bid->cnt, &(job.dst)); ROE ("SPWAW_UHT_list_init()");
+
+	for (unsigned int i=0; i<job.src.bid->cnt; i++) {
+		uhte = job.src.bid->list[i];
+		if (!SPWAW_UHT_is (uhte, &(job.src.bid->bdate), job.status)) continue;
+
+		rc = SPWAW_UHT_list_add (job.dst, uhte, &ulle); ROE ("SPWAW_UHT_list_add()");
+		ulle->count = 1;
+
+		if (job.data_cb) {
+			job.data_cb (uhte, &(job.src.bid->bdate), SPWAW_UHT_lookup (uhte, &(job.src.bid->bdate)), true, &(ulle->data));
+		}
+
+	}
+
+	return (SPWERR_OK);
+}
+
+SPWAWLIB_API SPWAW_ERROR
+SPWAW_UHT_list_job (SPWAW_UHT_LIST_JOB &job)
+{
+	SPWAW_ERROR		rc = SPWERR_OK;
+
+	switch (job.type) {
+		default:
+		case SPWAW_UHT_LIST_CHAIN:
+			rc = perform_uht_chain_list_job (job);
+			break;
+		case SPWAW_UHT_LIST_BINFO:
+			rc = perform_uht_binfo_list_job (job);
+			break;
+	}
+
+	return (rc);
+}
+
+SPWAWLIB_API SPWAW_ERROR
+SPWAW_UHT_list_job_sort (SPWAW_UHT_LIST_JOB &job, SPWAW_UHT_LIST_SORT_TYPE type, bool ascending, SPWAW_UHT_list_cmp cmp)
+{
+	return (SPWAW_UHT_list_sort (job.dst, type, ascending, cmp));
+}
