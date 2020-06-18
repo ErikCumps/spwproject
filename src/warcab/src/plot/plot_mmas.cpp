a284defefc7d352072fbe0bbf7cdb289b34e90e3
ErikCumps <erik.cumps@gmail.com>
2020-06-18 22:59:05 +0200

    Updated copyright statements.
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

--- src/warcab/src/plot/plot_mmas.cpp
+++ src/warcab/src/plot/plot_mmas.cpp
@@ -1,7 +1,7 @@
 /** \file
  * The SPWaW war cabinet - plot handling - min-max-average-spread plot handling.
  *
- * Copyright (C) 2005-2019 Erik Cumps <erik.cumps@gmail.com>
+ * Copyright (C) 2005-2020 Erik Cumps <erik.cumps@gmail.com>
  *
  * License: GPL v2
  */
