1e7e9a8c1486c539b86a1e8fbe88e18e43e1983a
ErikCumps <erik.cumps@gmail.com>
2020-10-22 22:26:00 +0200

    Improved SPWAW_PERIOD string formatting.
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

--- src/warcab/src/util/util.cpp
+++ src/warcab/src/util/util.cpp
@@ -1,7 +1,7 @@
 /** \file
  * The SPWaW war cabinet - utility functions.
  *
- * Copyright (C) 2005-2018 Erik Cumps <erik.cumps@gmail.com>
+ * Copyright (C) 2005-2020 Erik Cumps <erik.cumps@gmail.com>
  *
  * License: GPL v2
  */
@@ -21,22 +21,27 @@ UTIL_fmt_longspan (SPWAW_PERIOD *span, char *buf, int len)
 void
 UTIL_fmt_longspan (SPWAW_PERIOD *span, UtilStrbuf *buf)
 {
-	bool		f = true;
+	bool	f = true;
+	bool	done = false;
 
 	if (!span || !buf) return;
 
 	if (span->years) {
 		buf->printf ("%s%u year%s", f ? "" : ", ", span->years, (span->years == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->months) {
 		buf->printf ("%s%u month%s", f ? "" : ", ", span->months, (span->months == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->weeks) {
 		buf->printf ("%s%u week%s", f ? "" : ", ", span->weeks, (span->weeks == 1) ? "" : "s");
-		//f = false;
+		done = true;
 	}
+
+	if (!done) UTIL_fmt_shortspan (span, buf);
 }
 
 void
@@ -51,21 +56,24 @@ UTIL_fmt_shortspan (SPWAW_PERIOD *span, char *buf, int len)
 void
 UTIL_fmt_shortspan (SPWAW_PERIOD *span, UtilStrbuf *buf)
 {
-	bool		f = true;
+	bool	f = true;
+	bool	done = false;
 
 	if (!span || !buf) return;
 
 	if (span->days) {
 		buf->printf ("%s%u day%s", f ? "" : ", ", span->days, (span->days == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->hours) {
 		buf->printf ("%s%u hour%s", f ? "" : ", ", span->hours, (span->hours == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->minutes) {
 		buf->printf ("%s%u minute%s", f ? "" : ", ", span->minutes, (span->minutes == 1) ? "" : "s");
-		//f = false;
+		done = true;
 	}
 }
 
@@ -81,28 +89,33 @@ UTIL_fmt_fullspan (SPWAW_PERIOD *span, char *buf, int len)
 void
 UTIL_fmt_fullspan (SPWAW_PERIOD *span, UtilStrbuf *buf)
 {
-	bool		f = true;
+	bool	f = true;
+	bool	done = false;
 
 	if (!span || !buf) return;
 
 	if (span->years) {
 		buf->printf ("%s%u year%s", f ? "" : ", ", span->years, (span->years == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->months) {
 		buf->printf ("%s%u month%s", f ? "" : ", ", span->months, (span->months == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->days) {
 		buf->printf ("%s%u day%s", f ? "" : ", ", span->days, (span->days == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->hours) {
 		buf->printf ("%s%u hour%s", f ? "" : ", ", span->hours, (span->hours == 1) ? "" : "s");
 		f = false;
+		done = true;
 	}
 	if (span->minutes) {
 		buf->printf ("%s%u minute%s", f ? "" : ", ", span->minutes, (span->minutes == 1) ? "" : "s");
-		//f = false;
+		done = true;
 	}
 }
