90e4d2d39888bd81e46cc7d081b123f3e1212799
ErikCumps <erik.cumps@gmail.com>
2020-11-02 21:43:31 +0100

    Updated regression test data.
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

--- _TEST_/_REGRESSION_/_SAVEGAMES_/set017/report_all.bat
+++ _TEST_/_REGRESSION_/_SAVEGAMES_/set017/report_all.bat
@@ -2,3 +2,6 @@
 
 call ..\bin\report.bat GECTEST spwaw
 call ..\bin\report.bat 42 spwaw
+call ..\bin\report.bat longtest_000_start  spwaw
+call ..\bin\report.bat longtest_005_core_flag spwaw
+call ..\bin\report.bat longtest_011_draw spwaw
