8a32a341c9bfd2ad4839056699dd9ed690671cae
ErikCumps <erik.cumps@gmail.com>
2020-11-02 21:22:11 +0100

    Added oobdir to SPWAW_SAVEGAME_DESCRIPTOR.
    Signed-off-by: ErikCumps <erik.cumps@gmail.com>

--- src/spwawlib/src/gamefile/savegame_descriptor.cpp
+++ src/spwawlib/src/gamefile/savegame_descriptor.cpp
@@ -11,7 +11,7 @@
 #include "common/internal.h"
 
 static SPWAW_ERROR
-savegame_descriptor_init_core (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *path, STRTAB *stab)
+savegame_descriptor_init_core (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *oobdir, const char *path, STRTAB *stab)
 {
 	SPWAW_ERROR	rc;
 
@@ -19,9 +19,15 @@ savegame_descriptor_init_core (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE g
 	sgd.gametype	= gametype;
 	sgd.savetype	= savetype;
 	if (stab) {
+		sgd.oobdir = STRTAB_add (stab, (char *)oobdir);
+		if (!sgd.oobdir) FAILGOTO (SPWERR_FAILED, "STRTAB_add(oobdir)", handle_error);
+
 		sgd.path = STRTAB_add (stab, (char *)path);
 		if (!sgd.path) FAILGOTO (SPWERR_FAILED, "STRTAB_add(path)", handle_error);
 	} else {
+		sgd.oobdir = safe_strdup ((char *)oobdir);
+		COOMGOTO (sgd.oobdir, "savegame descriptor oobdir", handle_error);
+
 		sgd.path = safe_strdup ((char *)path);
 		COOMGOTO (sgd.path, "savegame descriptor path", handle_error);
 	}
@@ -34,11 +40,11 @@ handle_error:
 }
 
 SPWAW_ERROR
-savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *path, unsigned int id, STRTAB *stab)
+savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *oobdir, const char *path, unsigned int id, STRTAB *stab)
 {
 	SPWAW_ERROR	rc;
 
-	rc = savegame_descriptor_init_core (sgd, gametype, savetype, path, stab);
+	rc = savegame_descriptor_init_core (sgd, gametype, savetype, oobdir, path, stab);
 	ROE ("savegame_descriptor_init_core()");
 
 	sgd.numeric_id	= true;
@@ -48,11 +54,11 @@ savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gamety
 }
 
 SPWAW_ERROR
-savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *path, const char * id, STRTAB *stab)
+savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *oobdir, const char *path, const char * id, STRTAB *stab)
 {
 	SPWAW_ERROR	rc;
 
-	rc = savegame_descriptor_init_core (sgd, gametype, savetype, path, stab);
+	rc = savegame_descriptor_init_core (sgd, gametype, savetype, oobdir, path, stab);
 	ROE ("savegame_descriptor_init_core()");
 
 	sgd.numeric_id	= false;
@@ -72,13 +78,42 @@ handle_error:
 }
 
 SPWAW_ERROR
-savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, ULONG gametype, ULONG savetype, ULONG path, ULONG id, STRTAB *stab)
+savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_SAVEGAME_DESCRIPTOR &src, STRTAB *stab)
+{
+	SPWAW_ERROR	rc;
+
+	rc = savegame_descriptor_init_core (sgd, src.gametype, src.savetype, src.oobdir, src.path, stab);
+	ROE ("savegame_descriptor_init_core()");
+
+	sgd.numeric_id = src.numeric_id;
+	if (sgd.numeric_id) {
+		sgd.id.number = src.id.number;
+	} else {
+		if (stab) {
+			sgd.id.name = STRTAB_add (stab, (char *)src.id.name);
+			if (!sgd.id.name) FAILGOTO (SPWERR_FAILED, "STRTAB_add(src.id.name)", handle_error);
+		} else {
+			sgd.id.name = safe_strdup ((char *)src.id.name);
+			COOMGOTO (sgd.id.name, "savegame descriptor ID name", handle_error);
+		}
+	}
+
+	return (SPWERR_OK);
+
+handle_error:
+	SPWAW_savegame_descriptor_clear (sgd);
+	return (rc);
+}
+
+SPWAW_ERROR
+savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, ULONG gametype, ULONG savetype, ULONG oobdir, ULONG path, ULONG id, STRTAB *stab)
 {
 	CNULLARG (stab);
 
 	memset (&sgd, 0, sizeof (sgd));
 	sgd.gametype	= (SPWAW_GAME_TYPE)gametype;
 	sgd.savetype	= (SPWAW_SAVE_TYPE)savetype;
+	sgd.oobdir	= STRTAB_getstr (stab, oobdir);
 	sgd.path	= STRTAB_getstr (stab, path);
 	sgd.numeric_id	= false;
 	sgd.id.name	= STRTAB_getstr (stab, id);
@@ -89,6 +124,13 @@ savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, ULONG gametype, ULONG 
 void
 savegame_descriptor_clear (SPWAW_SAVEGAME_DESCRIPTOR &sgd, STRTAB *stab)
 {
+	if (sgd.oobdir) {
+		if (stab) {
+			STRTAB_del (stab, sgd.oobdir);
+		} else {
+			safe_free (sgd.oobdir);
+		}
+	}
 	if (sgd.path) {
 		if (stab) {
 			STRTAB_del (stab, sgd.path);
--- src/spwawlib/src/gamefile/savegame_descriptor.h
+++ src/spwawlib/src/gamefile/savegame_descriptor.h
@@ -12,9 +12,10 @@
 #include <spwawlib_types.h>
 #include "strtab/strtab.h"
 
-extern	SPWAW_ERROR	savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *path, unsigned int id, STRTAB *stab = NULL);
-extern	SPWAW_ERROR	savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *path, const char * id, STRTAB *stab = NULL);
-extern	SPWAW_ERROR	savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, ULONG gametype, ULONG savetype, ULONG path, ULONG id, STRTAB *stab);
+extern	SPWAW_ERROR	savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *oobdir, const char *path, unsigned int id, STRTAB *stab = NULL);
+extern	SPWAW_ERROR	savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_GAME_TYPE gametype, SPWAW_SAVE_TYPE savetype, const char *oobdir, const char *path, const char * id, STRTAB *stab = NULL);
+extern	SPWAW_ERROR	savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, SPWAW_SAVEGAME_DESCRIPTOR &src, STRTAB *stab = NULL);
+extern	SPWAW_ERROR	savegame_descriptor_init (SPWAW_SAVEGAME_DESCRIPTOR &sgd, ULONG gametype, ULONG savetype, ULONG oobdir, ULONG path, ULONG id, STRTAB *stab);
 extern	void		savegame_descriptor_clear (SPWAW_SAVEGAME_DESCRIPTOR &sgd, STRTAB *stab = NULL);
 
 #endif	/* SAVEGAME_DESCRIPTOR_H */
